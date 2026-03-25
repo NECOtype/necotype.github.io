72f7582f7c9d34c039dfaf56f25593a5b9870a8d
NECOdes <necodes@duck.com>
2026-03-25 20:25:55 +0100

    remove tailwindcss, back to good ol css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import svelte from '@astrojs/svelte';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-
-	integrations: [svelte()]
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Instrument Serif",
+			cssVariable: "--font-instrument",
+			fallbacks: ["serif"],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "IM Fell English",
+			cssVariable: "--font-im-fell",
+			fallbacks: ["serif"],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "IM Fell Great Primer",
+			cssVariable: "--font-im-fell-great",
+			fallbacks: ["serif"],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Yuji Syuku",
+			cssVariable: "--font-yuji-syuku",
+			fallbacks: ["serif"],
+		},
+	],
+	integrations: [],
 });
